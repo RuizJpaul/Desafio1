53c24464a9733d2b8e5c727043826d2a6e3a04bf
RuizJpaul <117695167+RuizJpaul@users.noreply.github.com>
2022-11-07 21:10:37 -0500

    Add files via upload

---
+++ ProductoVendido.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio1
+{
+    internal class ProductoVendido
+    {
+        private int id;
+        private string idProducto;
+        private int stock;
+        private string idVenta;
+    }
+}
---
+++ Usuario.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio1
+{
+    internal class Usuario
+    {
+        private int id;
+        private string nombre;
+        private string apellido;
+        private string nombreUsuario;
+        //Contraseña (numeros)
+        private int contraseña;
+        private string mail;
+    }
+}
